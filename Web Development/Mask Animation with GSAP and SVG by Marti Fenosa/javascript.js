f463507a11795d82633f564475dba2b7414590df
Billy <billyfong2007@hotmail.com>
2018-10-17 14:00:14 +0800

    Progress on 17 Oct 2018

--- Web Development/Mask Animation with GSAP and SVG by Marti Fenosa/javascript.js
+++ Web Development/Mask Animation with GSAP and SVG by Marti Fenosa/javascript.js
@@ -1,3 +1,11 @@
 function showAlertWindow() {
-  window.alert("showAlertWindow() ran");
+  window.alert('showAlertWindow() ran');
+}
+
+function readScreenWidth() {
+  console.log('Screen Width: ' + screen.width);
+}
+
+function readScreenHeight() {
+  console.log('Screen Height: ' + screen.height);
 }
