4b816bcb323e319d4aa674ea81b66eb75d3b6118
Billy <billyfong2007@hotmail.com>
2018-10-19 22:07:31 +0800

    Added javascript tutorial

--- Web Development/Mask Animation with GSAP and SVG by Marti Fenosa/javascript.js
+++ Web Development/Mask Animation with GSAP and SVG by Marti Fenosa/javascript.js
@@ -4,8 +4,10 @@ function showAlertWindow() {
 
 function readScreenWidth() {
   console.log('Screen Width: ' + screen.width);
+  document.getElementById('screenWidthOutput').value = 'Screen Width: ' + screen.width + 'px';
 }
 
 function readScreenHeight() {
   console.log('Screen Height: ' + screen.height);
+  document.getElementById('screenHeightOutput').value = 'Screen height: ' + screen.height + 'px';
 }
